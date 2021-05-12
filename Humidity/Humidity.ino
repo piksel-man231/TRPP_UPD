935d1c1b5ff9236703d9b608702482b3f9f0aadd
Kirill <piksel-man231@yandex.ru>
2021-05-12 17:19:52 +0300

    Реализована функция отображения влажности в помещении

--- Humidity/Humidity.ino
+++ Humidity/Humidity.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <QuadDisplay2.h>
+#include <TroykaDHT.h>
 
+QuadDisplay QDH(9,5,7);
+DHT sen(0, DHT11);
+
+void setup() {
+  QDH.begin();
+  sen.begin();
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop() {
+  sen.read();
+  Serial.print(sen.getHumidity());
+  QDH.displayHumidity(sen.getHumidity());
 }
