b5988eb4a6a11a838b94e9be7ecc430542272ad0
Kirill <piksel-man231@yandex.ru>
2021-05-12 17:59:51 +0300

    Объеденены бревна в 1 файл

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,35 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <TroykaButton.h>
+#include <QuadDisplay2.h>
+#include <TroykaDHT.h>
 
+TroykaButton btn(1);
+QuadDisplay QDH(9,5,7);
+QuadDisplay QDT(10,1,4);
+DHT sen(0, DHT11);
+
+ 
+void setup()
+{
+  Serial.begin(9600);
+  btn.begin();
+  QDH.begin();
+  QDT.begin();
+  sen.begin();
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+ 
+void loop()
+{
+  static bool flag = false;
+ if (btn.isClick()){
+    flag = !flag;
+    if (flag){
+        QDT.displayClear();
+        QDH.displayClear();
+    };
+    sen.read();
+    Serial.print(sen.getHumidity());
+    QDH.displayHumidity(sen.getHumidity());
+    Serial.print(sen.getTemperatureC());
+    QDT.displayTemperatureC(sen.getTemperatureC());
+  };
 }
